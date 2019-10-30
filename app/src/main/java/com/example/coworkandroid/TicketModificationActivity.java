dde11fc9663ad769b9392a4975c4fa96eda3c30b
Massicilia <massicilia.nedir@outlook.com>
2019-10-30 23:26:55 +0100

    ticket service

---
+++ app/src/main/java/com/example/coworkandroid/TicketModificationActivity.java
@@ -0,0 +1,13 @@
+package com.example.coworkandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicketModificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_modification);
+    }
+}
